76726cd06566abdbd0bad9e1e308b0f33f971205
saeed.usefee <saeed.usefee@gmail.com>
2022-04-02 20:56:54 +0430

    Now you can change status similar to state transitions

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,18 +1,55 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { updateTask } from "../@rechat/components/contetxProvider/TodoAction";
 import { TodoContext } from "../@rechat/components/contetxProvider/TodoContext";
+import { taskType } from "../@rechat/utils/constantTypes";
 
 
 const Edit = () => {
     const navigate = useNavigate();
 
-    const { currentTask }: any = useContext(TodoContext);
+    const { setTasks, currentTask }: any = useContext(TodoContext);
 
     const [taskTitle, setTaskTitle] = useState(currentTask?.title || '');
     const [taskDesription, setTaskDesription] = useState(currentTask?.description || '');
     const [statusTask, setStatusTask ] = useState(currentTask?.status || 'todo');
 
+    const onChangeStatus = (value: string) => {
+        /* Only the following status transitions are allowed */
+        if (currentTask?.status === 'todo' && value === 'InProgress') { 
+            // todo > InProgress
+            setStatusTask(value);
+        } else if (currentTask?.status === 'InProgress' && (value === 'blocked' || value === 'InQA')) { 
+            // blocked < InProgress > InQA
+            setStatusTask(value);
+        } else if (currentTask?.status === 'blocked' && value === 'todo') {
+            // blocked > todo
+            setStatusTask(value);
+        } else if (currentTask?.status === 'InQA' && (value === 'todo' || value === 'done')) {
+            // ToDo < InQA > done
+            setStatusTask(value);
+        } else if (currentTask?.status === 'done' && value === 'InQA') {
+            // Done > InQA ** it's just for pervent to locking status **
+            setStatusTask(value);
+        } else if (currentTask?.status === value){
+            // without change
+            setStatusTask(value);
+        }
+    };
+
+
+    const handleEdit = () => {
+        updateTask(setTasks, {
+            id: currentTask.id,
+            title: taskTitle,
+            description: taskDesription,
+            status: statusTask,
+        });
+        // navigate to the home after editing task
+        navigate('/');
+    };
+    
     return (
         <div className="page-root">
             <div className="container">
@@ -29,7 +66,8 @@ const Edit = () => {
                         placeholder="description"
                         value={taskDesription}/>
                     <select
-                        value={statusTask}>
+                        value={statusTask}
+                        onChange={(e) => onChangeStatus(e.target.value)}>
                         <option value={'blocked'}>Blocked</option>
                         <option value={'todo'}>ToDo</option>
                         <option value={'InProgress'}>InProgress</option>
@@ -37,7 +75,7 @@ const Edit = () => {
                         <option value={'done'}>Done</option>
                     </select>
                     <div className="edit-form-btn">
-                        <button >Edit</button>
+                        <button onClick={handleEdit}>Edit</button>
                         <button onClick={() => navigate('/')} className="white-button">Cancel</button>
                     </div>
                 </form>
